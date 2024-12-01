694cc1775aa44b654e16d3066d69ca420eeaaae4
Kyuhyeok Park <kh2000park@gmail.com>
2024-12-01 10:32:06 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/carrot_clone_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.carrot_clone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CarrotCloneApp());
+}
+
+class CarrotCloneApp extends StatelessWidget {
+  const CarrotCloneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
