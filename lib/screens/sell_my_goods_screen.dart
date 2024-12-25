3c07ff50d28eece87d389096fed84f7a61750193
Kyuhyeok Park <kh2000park@gmail.com>
2024-12-25 18:27:22 +0900

    add basic text input widgets to `SellMyGoodsScreen`

--- lib/screens/sell_my_goods_screen.dart
+++ lib/screens/sell_my_goods_screen.dart
@@ -10,6 +10,71 @@ class SellMyGoodsScreen extends StatefulWidget {
 class _SellMyGoodsScreenState extends State<SellMyGoodsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '내 물건 팔기',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.1,
+        shadowColor: Colors.black,
+        leading: IconButton(
+          onPressed: Navigator.of(context).pop, // tear-off
+          icon: const Icon(Icons.close),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              '임시저장',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 18,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const FormLabel('제목'),
+            const TextField(),
+            const SizedBox(height: 30),
+            const FormLabel('거래방식'),
+            const TextField(),
+            const SizedBox(height: 30),
+            const FormLabel('자세한 설명'),
+            const TextField(),
+            const SizedBox(height: 30),
+            FilledButton(onPressed: () {}, child: const Text('작성 완료'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FormLabel extends StatelessWidget {
+  const FormLabel(
+    this.labelText, {
+    super.key,
+  });
+
+  final String labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      labelText,
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 14,
+      ),
+    );
   }
 }
